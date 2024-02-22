ed59b848e813ec128da180e6d13c5158b9f42a61
DioneKx <mjhonnyvieira@gmail.com>
2024-02-22 19:03:05 -0300

    Jhonny: Aula-01

---
+++ aula01/aula01.py
@@ -0,0 +1,8 @@
+numero = int(input("Informe um número: ")) # O input de python sempre retorna em string, por isso, quando se trata de numero, faça a conversão
+numero2 = float(input("Informe um número: ")) # O tipo que tem maior quantidade de dados p/armazenar prevalece ao efetuar uma equação
+soma = numero + numero2 # Caso não tivesse convertido, essa var estaria concatenando as outras duas
+vt = "Valor Total"
+
+print(type(soma)) # type retorna o tipo da var dentro dos () 
+print(vt, "da soma", soma) # Para concatenar, por exemplo, em print, utilize "," após as aspas do texto ou a var (Pode-se usar tambem "+", porém deve converter os numeros para String)
+# Ao utilizar "%<Alguma formatação>", podemos usar alguma outra var para substituir esse trecho por outra coisa ao acrescentar "%" depois das aspas principais... (REVER)
